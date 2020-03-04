447272d1d912be2c247fce6f580764e52d0bc1d1
Yan <yanl2@egb076000013.lancs.local>
2020-03-04 14:26:20 +0000

    New changes implemented on Board and Square classes

--- Board.java
+++ Board.java
@@ -1,13 +1,31 @@
+import javax.swing.*;
 /** 
  * Model is a class that will use Square.java to build a Graphical User Interface(GUI)
  */
 
 public class Board
 {
-    public void addSquare()
-    {
-        //add jframe
-        Square n = new Square();
-        n.newSquare();
-    }
+    private String title;
+    private int numOfSquare = 10;
+    private int height, width;
+    public void addWindow(String c, int x, int y)
+        {
+            title = c;
+            height = x;
+            width = y;
+            JFrame window = new JFrame(title);
+            window.setSize(height, width);
+            window.setVisible(true);
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        }
+
+    public void addSquare(int n)
+        {
+            numOfSquare = n;
+            for (int i = 0; i < numOfSquare; i++)
+            {
+                Square a = new Square();
+                a.newSquare();
+            }
+        }
 }
