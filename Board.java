014b27f901ee5bdc5b94c0e599a2c2156864ec5c
lanxinyan <Lanxin-Yan@users.noreply.github.com>
2020-03-24 20:09:41 +0000

    Completion of task 4
    Task 4 completed, added logic of how frogs should move legally, however there was an error to be fixed...

--- Board.java
+++ Board.java
@@ -1,37 +1,106 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
-/** 
-* Model is a class that will use Square.java to build a Graphical User Interface(GUI)
-*/
 
-public class Board
+/**
+ * Model is a class that will use Square.java to build a Graphical User
+ * Interface(GUI)
+ */
+public class Board implements ActionListener // inherits all methods from Square class 
 {
-    //delared all the variables to be used
+
+    // declared all the variables to be used
     private JFrame window;
-    private JPanel panel;
+    public JPanel panel;
+    private Square a = new Square();
+    private ImageIcon greenFrog = a.greenFrog;
+    private ImageIcon greenFrog2 = a.greenFrog2;
+    private ImageIcon redFrog = a.redFrog;
+    private ImageIcon redFrog2 = a.redFrog2;
 
-    public Board()
-        {
-            window = new JFrame();
-            window.setTitle("Hoppers");
-            window.setSize(700, 700);
+    private ImageIcon lilypad = a.lilypad;
+    /*private ImageIcon greenFrog = new ImageIcon("GreenFrog.png");
+    private ImageIcon redFrog = new ImageIcon("RedFrog.png");
+    private ImageIcon greenFrog2 = new ImageIcon("GreenFrog2.png");
+    private ImageIcon redFrog2 = new ImageIcon("RedFrog2.png");*/
 
-            panel = new JPanel();
-            panel.setLayout(new GridLayout(5,5));
-            
-            JButton[] jb = new JButton[25];
+    public Board(int width, int height, String title) 
+    {
+        this.window = new JFrame();
+        this.window.setTitle(title);
+        this.window.setSize(width, height);
+        // added frame
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(5, 5));
+        // added panel
+        a.changeButton();
 
-            for (int i=0; i<25; i++)
-            {
-                ImageIcon a = new ImageIcon("Water.png");
-                jb[i] = new JButton(a);
-                jb[i].setIcon(a);
-                panel.add(jb[i]);
+        for (int x = 0; x < 5; x++) {
+            for (int y = 0; y < 5; y++) {
+                a.getSquare(x, y).addActionListener(this);
+                panel.add(a.getSquare(x, y));
+
             }
-
-            window.setContentPane(panel);
-            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            window.setVisible(true);
         }
-    //this method will open a window frame as the games GUI
+        window.setContentPane(panel); // adding panel to windown frame
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // set exit on close
+        window.setVisible(true); // set window frame visible
+    }
+
+ @Override
+    public void actionPerformed(ActionEvent e) 
+    {
+        if (((AbstractButton) e.getSource()).getIcon() == greenFrog)
+            {
+                System.out.println(""+((Component) e.getSource()).getBounds());
+                System.out.println(""+((AbstractButton) e.getSource()).getIcon());
+                for (int x = 0; x < 5; x++){
+                    for (int y = 0; y <5; y++){
+                        if (a.getSquare(x, y).getIcon() == greenFrog2){
+                            a.getSquare(x, y).setIcon(greenFrog);
+                        }
+                        else if (a.getSquare(x, y).getIcon() == redFrog2){
+                            a.getSquare(x, y).setIcon(redFrog);
+                        }
+                    }
+                }
+                ((AbstractButton) e.getSource()).setIcon(greenFrog2);
+            }
+        else if (((AbstractButton) e.getSource()).getIcon() == greenFrog2)
+            {
+                ((AbstractButton) e.getSource()).setIcon(greenFrog);
+                System.out.println(""+((Component) e.getSource()).getBounds());
+                System.out.println(""+((AbstractButton) e.getSource()).getIcon());
+            }
+        else if (((AbstractButton) e.getSource()).getIcon() == redFrog)
+            {
+                for (int x = 0; x < 5; x++){
+                    for (int y = 0; y <5; y++){
+                        if (a.getSquare(x, y).getIcon() == greenFrog2){
+                            a.getSquare(x, y).setIcon(greenFrog);
+                        }
+                    }
+                }
+                ((AbstractButton) e.getSource()).setIcon(redFrog2);
+                System.out.println(""+((Component) e.getSource()).getBounds());
+                System.out.println(""+((AbstractButton) e.getSource()).getIcon());
+            }
+        else if (((AbstractButton) e.getSource()).getIcon() == redFrog2)
+            {
+                ((AbstractButton) e.getSource()).setIcon(redFrog);
+                System.out.println(""+((Component) e.getSource()).getBounds());
+                System.out.println(""+((AbstractButton) e.getSource()).getIcon());
+            }
+        
+        else if (((AbstractButton) e.getSource()).getIcon() == lilypad)
+            {
+            a.moveTo((JButton) e.getSource());
+            }
+    }
+
+    public static void main(String[] args) 
+    {
+        Board c = new Board(750, 750, "Hopper!");
+    }
 }
--- Square.java
+++ Square.java
@@ -1,17 +1,164 @@
 import javax.swing.*;
-import java.awt.*;
+import java.awt.Rectangle;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
 
-/** 
- * Model is a clickable suqare that will be used later on in my GUI
- * This class represents a Square objects, when combined with the Board.java class,
+/**
+ * Model is a clickable square that will be used later on in my GUI This class
+ * represents a Square objects, when combined with the Board.java class,
  * Instances of the Square class can be displayed on the screen.
  */
 
-public class Square
-{
-    public Square(){
-        JButton jb = new JButton();
-        
-        jb.setIcon(new ImageIcon("Water.png"));
+public class Square extends JButton {
+    private static final long serialVersionUID = 1L;
+    public ImageIcon water = new ImageIcon("Water.png");
+    public ImageIcon lilypad = new ImageIcon("Lilypad.png");
+    public ImageIcon greenFrog = new ImageIcon("GreenFrog.png");
+    public ImageIcon redFrog = new ImageIcon("RedFrog.png");
+    public ImageIcon greenFrog2 = new ImageIcon("GreenFrog2.png");
+    public ImageIcon redFrog2 = new ImageIcon("RedFrog2.png");
+
+    private JButton[][] square = new JButton[5][5];
+    private String[][] Loc = new String[5][5];
+
+    private Rectangle lily;
+    private Rectangle highlightedFrog;
+    //private Rectangle middleFrog;
+    //private Rectangle abstractRectagle;
+
+    private Boolean redOrGreen;
+
+    private int highlightXLoc;
+    private int highlightYLoc;
+    private int xLoc;
+    private int yLoc;
+    private int middlexLoc;
+    private int middleyLoc;
+
+    public Square() {
+        for (int i = 0; i < square.length; i++)// use a for loop to add the button 25 times
+        {
+            for (int j = 0; j < square[i].length; j++) {
+                square[i][j] = new JButton();
+            }
+        }
     }
+
+    public JButton getSquare(int x, int y) {
+        return square[x][y];
+    }
+
+    public void changeButton() {
+        try {
+            Scanner sc = new Scanner(new BufferedReader((new FileReader("level1.txt"))));
+            String[][] myArray = new String[5][5];
+            while (sc.hasNextLine()) {
+                for (int x = 0; x < myArray.length; x++) {
+                    String[] line = sc.nextLine().trim().split(" ");
+                    for (int y = 0; y < line.length; y++) {
+                        myArray[x][y] = line[y];
+
+                        switch (myArray[x][y]) {
+
+                            case "g":
+                                Loc[x][y] = "greenFrog";
+                                square[x][y].setIcon(greenFrog);
+                                break;
+
+                            case "r":
+                                Loc[x][y] = "redFrog";
+                                square[x][y].setIcon(redFrog);
+                                break;
+
+                            case "l":
+                                Loc[x][y] = "lilypad";
+                                square[x][y].setIcon(lilypad);
+                                break;
+
+                            case "w":
+                                Loc[x][y] = "water";
+                                square[x][y].setIcon(water);
+                                break;
+
+                            default:
+                                Loc[x][y] = "water";
+                                square[x][y].setIcon(water);
+                        }
+                    }
+                }
+            }
+        }
+
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void moveTo(JButton XY) 
+    {
+        System.out.println("" + XY.getIcon());
+        System.out.println("" + XY.getBounds());
+
+        lily = XY.getBounds();
+        xLoc = lily.x;
+        yLoc = lily.y;
+
+        for (int i = 0; i < 5; i++){
+            for (int y = 0; y < 5; y++)
+            {
+                if (square[i][y].getIcon() == redFrog2){
+                    highlightedFrog = square[i][y].getBounds();
+                    highlightXLoc = highlightedFrog.x;
+                    highlightYLoc = highlightedFrog.y;
+                    redOrGreen = false;
+                }
+                else if (square[i][y].getIcon() == greenFrog2){
+                    highlightedFrog = square[i][y].getBounds();
+                    highlightXLoc = highlightedFrog.x;
+                    highlightYLoc = highlightedFrog.y;
+                    redOrGreen = true;
+                }
+            }
+        }
+
+        middlexLoc = (xLoc + highlightXLoc) / 2;
+        middleyLoc = (yLoc + highlightYLoc) / 2;
+
+        for (int x = 0; x < 5; x++){
+            for (int j = 0; j < 5; j++){
+                if (square[x][j].getIcon() == greenFrog){
+                    if (square[x][j].getBounds().x == middlexLoc && square[x][j].getBounds().y == middleyLoc){
+                        square[x][j].setIcon(lilypad);
+                        if (redOrGreen = true){
+                            XY.setIcon(greenFrog);
+                            System.out.println("" + redOrGreen);
+                        }
+                        else if (redOrGreen = false){
+                            XY.setIcon(redFrog);
+                            System.out.println("" + redOrGreen);
+                        }
+                            for (int i = 0; i < 5; i++){
+                                for (int y = 0; y < 5; y++){
+                                    if (square[i][y].getIcon() == redFrog2 || square[i][y].getIcon() == greenFrog2){
+                                        square[i][y].setIcon(lilypad);
+                                    }
+                                }
+                            }
+                        }
+                    else{
+                        System.out.println("FAKE");
+                    }
+                }
+                else{
+                    System.out.println("OH no");                    
+                }
+            }
+        }
+        System.out.println("" + highlightXLoc);
+        System.out.println("" + highlightYLoc);
+        System.out.println("" + xLoc);
+        System.out.println("" + yLoc);
+	}
 }
