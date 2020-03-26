3e2dc1af3a9d1b7f9a28d248feebc97e96190232
lanxinyan <Lanxin-Yan@users.noreply.github.com>
2020-03-26 20:57:22 +0000

    Completion of Javadoc
    Improved code elegance, structure.
    Have uniformed the code style, used indentation, camel case etc.
    
    Commented on complexed algorithms in methods.

--- Board.java
+++ Board.java
@@ -7,49 +7,65 @@ import javax.swing.*;
  * Model is a class that will use Square.java to build a Graphical User
  * Interface(GUI)
  */
-public class Board implements ActionListener // inherits all methods from Square class 
-{
+public class Board implements ActionListener{ // inherits all methods from Square class 
 
     // declared all the variables to be used
     private JFrame window;
     public JPanel panel;
+
     private Square a = new Square();
+    //innitiating a new instance of the class Square
+
     private ImageIcon greenFrog = a.greenFrog;
     private ImageIcon greenFrog2 = a.greenFrog2;
     private ImageIcon redFrog = a.redFrog;
     private ImageIcon redFrog2 = a.redFrog2;
     private ImageIcon lilypad = a.lilypad;
+    //pull the ImageIcon variables over from Square.class
 
-    public Board(int width, int height, String title) 
-    {
+    /**
+     * Constructor, a JFrame window will be build with the parameter: int, int, String
+     * a JPanel is created and placed inside the JFrame
+     * JPanel contains the actual game with 25 buttons
+     */
+
+    public Board(int width, int height, String title){
+
         this.window = new JFrame();
         this.window.setTitle(title);
         this.window.setSize(width, height);
-        // added frame
+        // added JFrame window
+
         panel = new JPanel();
         panel.setLayout(new GridLayout(5, 5));
-        // added panel
-       // a.levelSelector();
-        a.changeButton();
+        // added panel with the layout of 5 x 5
 
-        for (int x = 0; x < 5; x++) {
-            for (int y = 0; y < 5; y++) {
+        a.changeButton();
+        //calls the function in Square, which will read the selected level in the text file
+
+        for (int x = 0; x < 5; x++){
+            for (int y = 0; y < 5; y++){
                 a.getSquare(x, y).addActionListener(this);
                 panel.add(a.getSquare(x, y));
+                //for every JButtons added, we have implemented an actionListener to it. 
             }
         }
-        window.setContentPane(panel); // adding panel to windown frame
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // set exit on close
-        window.setVisible(true); // set window frame visible
+        //I have used my getSquare method to return my JButtons onto the 5x5 JPanel
+
+        window.setContentPane(panel);
+         // assigning panel to windown frame
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         // set exit on close action
+        window.setVisible(true);
+         // set window frame visible
     }
 
+    /**
+     * What action is performed when a buttom is clicked
+     */
  @Override
-    public void actionPerformed(ActionEvent e) 
-    {
-        if (((AbstractButton) e.getSource()).getIcon() == greenFrog)
-            {
-                System.out.println(""+((Component) e.getSource()).getBounds());
-                System.out.println(""+((AbstractButton) e.getSource()).getIcon());
+    public void actionPerformed(ActionEvent e){
+        if (((AbstractButton) e.getSource()).getIcon() == greenFrog){
                 for (int x = 0; x < 5; x++){
                     for (int y = 0; y <5; y++){
                         if (a.getSquare(x, y).getIcon() == greenFrog2){
@@ -62,14 +78,15 @@ public class Board implements ActionListener // inherits all methods from Square
                 }
                 ((AbstractButton) e.getSource()).setIcon(greenFrog2);
             }
-        else if (((AbstractButton) e.getSource()).getIcon() == greenFrog2)
-            {
+            //when a green frog is clicked, set the according icon to highlighted greenfrog, which is greenfrog2, however, if there is any other highlighted green frog on board, set 
+            //it back to greenfrog, so there isn't more than 2 highlighted frog on board
+
+        else if (((AbstractButton) e.getSource()).getIcon() == greenFrog2){
                 ((AbstractButton) e.getSource()).setIcon(greenFrog);
-                System.out.println(""+((Component) e.getSource()).getBounds());
-                System.out.println(""+((AbstractButton) e.getSource()).getIcon());
             }
-        else if (((AbstractButton) e.getSource()).getIcon() == redFrog)
-            {
+            //when a highlighted greenfrog is clicked, remove its highlight status, for which I have just set its icon back to greenfrog
+
+        else if (((AbstractButton) e.getSource()).getIcon() == redFrog){
                 for (int x = 0; x < 5; x++){
                     for (int y = 0; y <5; y++){
                         if (a.getSquare(x, y).getIcon() == greenFrog2){
@@ -78,24 +95,27 @@ public class Board implements ActionListener // inherits all methods from Square
                     }
                 }
                 ((AbstractButton) e.getSource()).setIcon(redFrog2);
-                System.out.println(""+((Component) e.getSource()).getBounds());
-                System.out.println(""+((AbstractButton) e.getSource()).getIcon());
             }
-        else if (((AbstractButton) e.getSource()).getIcon() == redFrog2)
-            {
+            //when a red frog is clicked, set the according icon to highlighted redfrog, which is redfrog2, however, if there is any greenfrog highlighted on board, set it to greenfrog
+            //so the situation of highlighted red frog and greenfrog can not happen
+
+        else if (((AbstractButton) e.getSource()).getIcon() == redFrog2){
                 ((AbstractButton) e.getSource()).setIcon(redFrog);
-                System.out.println(""+((Component) e.getSource()).getBounds());
-                System.out.println(""+((AbstractButton) e.getSource()).getIcon());
             }
+            //when a highlighted red frog is clicked, set it to redfrog
         
-        else if (((AbstractButton) e.getSource()).getIcon() == lilypad)
-            {
+        else if (((AbstractButton) e.getSource()).getIcon() == lilypad){
             a.moveTo((JButton) e.getSource());
             }
+            //when a lilypad is clicked, jump to the moveTo method in Square class, which contains the logic to allow legal moves. 
     }
 
-    public static void main(String[] args) 
-    {
-        Board c = new Board(750, 750, "Hopper!");
+    /**
+     * Main function, which passes values into the Board.class' constructor, allow it to use
+     * those elements to build the GUI.
+     */
+    public static void main(String[] args){
+        Board c = new Board(750, 750, "Hopper!");   
+        //the JFrame window would be 750x750, with the title Hopper!
     }
 }
--- Square.java
+++ Square.java
@@ -13,22 +13,25 @@ import javax.swing.JOptionPane;
 
 public class Square extends JButton {
     private static final long serialVersionUID = 1L;
+    //inherited all the functions from JButton library 
+
     public ImageIcon water = new ImageIcon("Water.png");
     public ImageIcon lilypad = new ImageIcon("Lilypad.png");
     public ImageIcon greenFrog = new ImageIcon("GreenFrog.png");
     public ImageIcon redFrog = new ImageIcon("RedFrog.png");
     public ImageIcon greenFrog2 = new ImageIcon("GreenFrog2.png");
     public ImageIcon redFrog2 = new ImageIcon("RedFrog2.png");
+    //innitiated ImageIcons, which searches file names through the directory by a String of txt
 
     private JButton[][] square = new JButton[5][5];
-    private String[][] Loc = new String[5][5];
+    //created a 2D array of JButtons
 
     private Rectangle lily;
     private Rectangle highlightedFrog;
-    //private Rectangle middleFrog;
-    //private Rectangle abstractRectagle;
+    //I have then created 2 rectangle object, which will allow me to get my x and y coord of my JButtons using getbounds function
 
     private int redOrGreen;
+    //This int variable will determine if a redfrog or yellow frog was selected
 
     private int highlightXLoc;
     private int highlightYLoc;
@@ -36,71 +39,99 @@ public class Square extends JButton {
     private int yLoc;
     private int middlexLoc;
     private int middleyLoc;
+    //integer variables which stores the x and y of the lilypad that has been clicked, the selected highlighted greenfrogs/redfrogs, and the button between.
 
-    public Square() {
-        for (int i = 0; i < square.length; i++)// use a for loop to add the button 25 times
-        {
-            for (int j = 0; j < square[i].length; j++) {
+    /**
+     * Constructor of the square class, which creates 25 JButtons, using 2 for loops
+     */
+    public Square(){
+
+        for (int i = 0; i < square.length; i++){
+
+            for (int j = 0; j < square[i].length; j++){
+                
                 square[i][j] = new JButton();
             }
         }
     }
 
-    public JButton getSquare(int x, int y) {
+    /**
+     * Get function that returns the JButton, which will be called in the Board class.
+     * @param x will be called in Board class using a for loop
+     * @param y will be called in Board class using a for loop
+     * @return returns the JButton in the desired x and y
+     */
+    public JButton getSquare(int x, int y){
+
         return square[x][y];
     }
 
-    
+    /**
+     * Added a levelSelector which will be called in the changeButton() method, it will return the desired level as a String value. 
+     * @return A string value that describes the choosen level 
+     */
     public String levelSelector(){
+
         String[] choices = { 
+
         "level1.txt", "level2.txt", "level3.txt", "level4.txt", "level5.txt", "level6.txt", "level7.txt","level8.txt","level9.txt","level20.txt","level21.txt",
         "level22.txt","level23.txt","level24.txt", "level25.txt","level26.txt","level27.txt","level28.txt","level29.txt","level30.txt","level31.txt","level32.txt","level33.txt",
         "level34.txt","level35.txt","level36.txt","level37.txt","level38.txt","level39.txt","level40.txt"
     };
+    //added an array of strings, each element describes a level 
+
         String input = (String) JOptionPane.showInputDialog(null, "Choose now...",
             "The Choice of a Lifetime", JOptionPane.QUESTION_MESSAGE, null, // Use
                                                                             // default
                                                                             // icon
             choices, // Array of choices
             choices[1]); // Initial choice
-        System.out.println(input);
         return input;
+        //return the string input which describe the level
     }
 
-    public void changeButton() {
-        try {
+    /**
+     * A method which uses a switch case to innitiate what appropriate icon goes on each JButtons, using a 
+     * switch case.
+     * A 2D array is innitiated here to store the content read from the txt file. This 2D array will be used
+     * as the switch target.
+     * Error handling is also added, with a try catch method, which will output an error message when file isn't
+     * found.
+     */
+    public void changeButton(){
+
+        try{
+
             Scanner sc = new Scanner(new BufferedReader((new FileReader(levelSelector()))));
             String[][] myArray = new String[5][5];
-            while (sc.hasNextLine()) {
-                for (int x = 0; x < myArray.length; x++) {
+            while (sc.hasNextLine()){
+
+                for (int x = 0; x < myArray.length; x++){
+
                     String[] line = sc.nextLine().trim().split(" ");
-                    for (int y = 0; y < line.length; y++) {
+                    for (int y = 0; y < line.length; y++){
+
                         myArray[x][y] = line[y];
 
-                        switch (myArray[x][y]) {
+                        switch (myArray[x][y]){
 
                             case "g":
-                                Loc[x][y] = "greenFrog";
                                 square[x][y].setIcon(greenFrog);
                                 break;
 
                             case "r":
-                                Loc[x][y] = "redFrog";
                                 square[x][y].setIcon(redFrog);
                                 break;
 
                             case "l":
-                                Loc[x][y] = "lilypad";
                                 square[x][y].setIcon(lilypad);
                                 break;
 
                             case "w":
-                                Loc[x][y] = "water";
                                 square[x][y].setIcon(water);
                                 break;
 
                             default:
-                                Loc[x][y] = "water";
                                 square[x][y].setIcon(water);
                         }
                     }
@@ -108,30 +139,37 @@ public class Square extends JButton {
             }
         }
 
-        catch (FileNotFoundException e) {
+        catch (FileNotFoundException e){
+
             e.printStackTrace();
         }
     }
 
-    public void moveTo(JButton XY) 
-    {
-        System.out.println("" + XY.getIcon());
-        System.out.println("" + XY.getBounds());
+    /**
+     * MoveTo function, which will only allow legal moves on board.
+     * @param XY the JButton thats passed in from actionlistener, which is the lilypad that has been clicked.
+     */
+    public void moveTo(JButton XY){
 
         lily = XY.getBounds();
         xLoc = lily.x;
         yLoc = lily.y;
+        //In order to get the x and y coords of the lilypad, I have passed it into a rectangle object.
+        //This allows me to do calculations with the X and Y.
 
         for (int i = 0; i < 5; i++){
-            for (int y = 0; y < 5; y++)
-            {
+
+            for (int y = 0; y < 5; y++){
+
                 if (square[i][y].getIcon() == redFrog2){
+
                     highlightedFrog = square[i][y].getBounds();
                     highlightXLoc = highlightedFrog.x;
                     highlightYLoc = highlightedFrog.y;
                     redOrGreen = 1;
                 }
                 else if (square[i][y].getIcon() == greenFrog2){
+
                     highlightedFrog = square[i][y].getBounds();
                     highlightXLoc = highlightedFrog.x;
                     highlightYLoc = highlightedFrog.y;
@@ -139,25 +177,36 @@ public class Square extends JButton {
                 }
             }
         }
+        //Search through the board, to check if there was a redfrog clicked or a greenfrog, using an interger 
+        //to determine this status. 
+        //Then also pass the x and y coords to another rectangle object, which I can then use to do calculations with
 
         middlexLoc = (xLoc + highlightXLoc) / 2;
         middleyLoc = (yLoc + highlightYLoc) / 2;
+        //to work out the middle coords, we add up the x of the highlighted button and clicked lilypad, divid it by 2
+        //to work out the middle coords, we add up the y of the highlighted button and clicked lilypad, divid it by 2
 
         for (int x = 0; x < 5; x++){
+
             for (int j = 0; j < 5; j++){
+
                     if (square[x][j].getIcon() == greenFrog){
+
                         if (square[x][j].getBounds().x == middlexLoc && square[x][j].getBounds().y == middleyLoc){
+
                             square[x][j].setIcon(lilypad);
                             if (redOrGreen == 0){
+
                                 XY.setIcon(greenFrog);
-                                System.out.println("" + redOrGreen);
                             }
                             else if (redOrGreen == 1){
-                                XY.setIcon(redFrog);
-                                System.out.println("" + redOrGreen);
+
+                                XY.setIcon(redFrog);                            
                             }
                                 for (int i = 0; i < 5; i++){
+
                                     for (int y = 0; y < 5; y++){
+
                                         if (square[i][y].getIcon() == redFrog2 || square[i][y].getIcon() == greenFrog2){
                                             square[i][y].setIcon(lilypad);
                                         }
@@ -165,17 +214,14 @@ public class Square extends JButton {
                                 }
                             }
                         else{
-                            System.out.println("FAKE");
                         }
                     }
-                    else{
-                        System.out.println("OH no");                    
+                    else{                   
                     }
+                }
             }
-        }
-        System.out.println("" + highlightXLoc);
-        System.out.println("" + highlightYLoc);
-        System.out.println("" + xLoc);
-        System.out.println("" + yLoc);
+            //Scan throught the board, to find the square inbetween the lilybutton and highlighted frog button.
+            //Check if this is a greenfrog, because frogs can only jump over greenfrogs
+            //If there is a greenfrog inbetween, swap the highlighted frog and lilypad, and remove the frog inbetween. 
 	}
 }
